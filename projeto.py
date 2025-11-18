7e62919c9e02807fc3a8a7aa8161096929774379
SimaoCT <casanova2004@sapo.pt>
2025-11-18 11:24:23 +0000

    É o in

---
+++ projeto.py
@@ -0,0 +1,10 @@
+#Library imports:
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as mp
+
+#Import dataset
+
+df = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv", encoding= "utf-8")
+print(df)
